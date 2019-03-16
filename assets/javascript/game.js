77c3fcdb344b6d80318398bfb17b0c7ae272edd3
Matthew Taube <taubemj@gmail.com>
2019-03-16 19:14:08 -0400

    working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,35 +1,88 @@
 
-$(document).ready(function() {
-var wins = 0
-var losses = 0
-var score = 0
-var computerNumber = randomNumber
+$(document).ready(function () {
 
-    $("#number").text(computerNumber)
+    var wins = 0
+    var losses = 0
+    var score = 0
+    
+    
+    
+    function getRndInteger(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    var computerNumber = getRndInteger(19, 120)
+
+    $("#random").text(computerNumber)
+
+    var gemBlue = Math.floor(Math.random() * 12) + 1;
+    console.log(gemBlue);
+
+    var gemPurple = Math.floor(Math.random() * 12) + 1;
+    console.log(gemPurple);
+
+    var gemYellow = Math.floor(Math.random() * 12) + 1;
+    console.log(gemYellow);
+
+    var gemGreen = Math.floor(Math.random() * 12) + 1;
+    console.log(gemGreen);
 
 
-var randomNumber = Math.floor(Math.random() * 101) + 19;
-console.log(randomNumber)
+    $("#blue").on("click", function () {
+    //Grab value of blue and add to the variable score, update score on page
+        score = gemBlue + score
+        $("#score").text(score)
+        updateScore(score)
+        checkWins()
 
-var gemBlue = Math.floor(Math.random() * 12) + 1;
-console.log(gemBlue);
+    })
+    $("#green").on("click", function () {
+        score = gemGreen + score
+        $("#score").text(score)
+        updateScore(score)
+        checkWins()
 
-var gemPurple = Math.floor(Math.random() * 12) + 1;
-console.log(gemPurple);
+    })
+    $("#purple").on("click", function () {
+        score = gemPurple + score
+        $("#score").text(score)
+        updateScore(score)
+        checkWins()
 
-var gemYellow = Math.floor(Math.random() * 12) + 1;
-console.log(gemYellow);
+    })
+    $("#yellow").on("click", function () {
+        score = gemYellow + score
+        $("#score").text(score)
+        updateScore(score)
+        checkWins()
 
-var gemGreen = Math.floor(Math.random() * 12) + 1;
-console.log(gemGreen);
+    })
 
+    function updateScore(x) {
+        $("#score").text(x)
 
-// $("#gem-blue").on("click", function {
-//  gemBlue + score
-// })
-// $("#gem-purple").on("click", function {
-
-// }
-
-// })
+    }
+    function checkWins(tally) {
+        if (score === computerNumber) {
+            wins ++;
+            $("#wins").text("Wins: " + wins)
+            reset()
+        }
+        else if (score > computerNumber) {
+            losses ++;
+            $("#losses").text("Losses: " + losses)
+            reset()
+        }
+    }
+    function reset() {
+        score = 0
+        computerNumber = getRndInteger(19, 120);
+        $("#random").text(computerNumber);
+        $("#score").text(score);
+        gemBlue = Math.floor(Math.random() * 12) + 1;
+        gemPurple = Math.floor(Math.random() * 12) + 1;
+        gemYellow = Math.floor(Math.random() * 12) + 1;
+        gemGreen = Math.floor(Math.random() * 12) + 1;
+    
+    }
+  
 })
